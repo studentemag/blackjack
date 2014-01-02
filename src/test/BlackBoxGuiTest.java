7de6f858d2686200169872c0eb7620ef5a81fe7e
galanogiuseppe <galanogiuseppe@gmail.com>
2014-01-02 22:45:26 +0100

    Test cases modified

--- src/test/BlackBoxGuiTest.java
+++ src/test/BlackBoxGuiTest.java
@@ -9,7 +9,7 @@ import org.uispec4j.interception.MainClassAdapter;
 import org.uispec4j.interception.WindowHandler;
 import org.uispec4j.interception.WindowInterceptor;
 
-import test.util.BlackJackOracleUtil;
+import test.util.BjOracle;
 import blackjack.Black;
 
 public class BlackBoxGuiTest extends UISpecTestCase {
@@ -18,8 +18,9 @@ public class BlackBoxGuiTest extends UISpecTestCase {
 	Button startButton, hitButton, standButton;
 	
 	private enum State {
-		START, ERROR, PLAY, UNDEFINED
+		INIT, ERROR, PLAY, UNDEFINED
 	};
+	
 	
 	protected void setUp() throws Exception {
 		//	We then tell this class that it needs to run the blackjack application 
@@ -39,37 +40,86 @@ public class BlackBoxGuiTest extends UISpecTestCase {
 	    
 	}
 	
+	/*
+	 * Riconosce lo stato corrente analizzando gli elementi dell'interfaccia grafica
+	 */
 	private State currentState(){
 		State state;
 		
 		if (dialog != null && 
 			dialog.isVisible().isTrue() && 
-			dialog.containsLabel("Push start!").isTrue()
+			dialog.containsLabel("push start!").isTrue()
 		)
 			state = State.ERROR;
 		
 		else if (!area.getText().isEmpty() &&
-			(field.getText().equalsIgnoreCase("Blackjack") || field.getText().isEmpty())
+			(field.getText().equalsIgnoreCase("BlackJack!") || field.getText().isEmpty())
 		)
 			state = State.PLAY;
 		
 		else if (area.getText().isEmpty() ||
 				 field.getText().equalsIgnoreCase("Win!")    ||
 				 field.getText().equalsIgnoreCase("Lose!")   ||
-				 field.getText().equalsIgnoreCase("Busted!")
-				 
+				 field.getText().equalsIgnoreCase("Busted!")  ||
+				 field.getText().equalsIgnoreCase("21 Vittoria Grande Baldoria!")		 
 		)
-			state = State.START;
+			state = State.INIT;
 		else
 			state = State.UNDEFINED;
 		
 		return state;
 	}
 	
+	/*
+	 * Transition 1
+	 */
+	public void testTransition1(){	
+		assertEquals(State.INIT,currentState());
+		boolean traversed = false;
+		do {
+			startButton.click();
+			if ( BjOracle.blackJack(area.getText()) )
+				standButton.click();
+			else
+				traversed=true;
+			
+		} while ( !traversed );
+		
+		assertFalse(area.getText().isEmpty());
+		assertEquals(State.PLAY,currentState());
+		assertTrue(field.getText().isEmpty());
+	}
 	
-	public void testErrorDialog1(){
+	/*
+	 * Transition 2
+	 */
+	public void testTransition2(){
+		assertEquals(State.INIT,currentState());
+		int cpu_global_score=0;
+		boolean traversed = false;
+		
+		do {
+			cpu_global_score=BjOracle.cpuGlobalScore(area_score.getText());
+			startButton.click();
+			if ( !BjOracle.blackJack(area.getText()) )
+				startButton.click();
+			else
+				traversed=true;
+			
+		} while ( !traversed );
+		
+		assertEquals(State.INIT,currentState());
+		assertEquals(BjOracle.bjWinString,field.getText());
+		assertEquals(cpu_global_score+1,BjOracle.cpuGlobalScore(area_score.getText()));
+	}
+	
+	
+	/*
+	 * Transitions 3 and 5
+	 */
+	public void testTransitions3and5(){
 		//Check that current state is start
-		assertEquals(State.START,currentState());
+		assertEquals(State.INIT,currentState());
 		
 		//	Modal dialogs are more difficult to handle than their non-modal counterparts, 
 		//	because the application code is blocked while the dialog is being shown, 
@@ -103,12 +153,15 @@ public class BlackBoxGuiTest extends UISpecTestCase {
 		
 		// Check that the dialog is now hidden and that current state is start
 		assertFalse(dialog.isVisible().isTrue());
-		assertEquals(State.START,currentState());
+		assertEquals(State.INIT,currentState());
 		
 	}
 	
-	public void testErrorDialog2(){
-		assertEquals(State.START,currentState());
+	/*
+	 * Transitions 4 and 5
+	 */
+	public void testTransitions4and5(){
+		assertEquals(State.INIT,currentState());
 		WindowInterceptor
 			.init(standButton.triggerClick())
 			.process( new WindowHandler() {
@@ -121,119 +174,143 @@ public class BlackBoxGuiTest extends UISpecTestCase {
 			)
 			.run();
 		assertFalse(dialog.isVisible().isTrue());
-		assertEquals(State.START,currentState());
+		assertEquals(State.INIT,currentState());
 	}
 	
-	public void testStart1(){
-		assertEquals(State.START,currentState());
+	/*
+	 * Transition 6
+	 */
+	public void testTransition6(){
+		testTransition1();
+		assertEquals(State.PLAY,currentState());		
 		do {
 			startButton.click();
-			assertFalse(area.getText().isEmpty());
-			assertEquals(State.PLAY,currentState());
-			if (!BlackJackOracleUtil.blackJack(area.getText()))
-				assertTrue(field.getText().isEmpty());
 		}
-		while (BlackJackOracleUtil.blackJack(area.getText()));
+		while (BjOracle.blackJack(area.getText()));
+		
+		assertEquals(State.PLAY,currentState());
+
+		assertFalse(area.getText().isEmpty());
+		assertTrue(field.getText().isEmpty());
 	}
 	
-	public void testStart2(){
-		assertEquals(State.START,currentState());
+	/*
+	 * Transition 7
+	 */
+	public void testTransition7(){
+		testTransition1();
+		assertEquals(State.PLAY,currentState());
+		
+		int player_global_score=BjOracle.playerGlobalScore(area_score.getText());
 		do {
 			startButton.click();
-			assertFalse(area.getText().isEmpty());
-			assertEquals(State.PLAY,currentState());
-			if (BlackJackOracleUtil.blackJack(area.getText()))
-				assertEquals("Blackjack",field.getText());
 		}
-		while (!BlackJackOracleUtil.blackJack(area.getText()));
+		while (!BjOracle.blackJack(area.getText()));
+		assertEquals(State.INIT,currentState());
+		
+		assertFalse(area.getText().isEmpty());
+		assertEquals(BjOracle.bjWinString,field.getText());
+		assertEquals(player_global_score+1,BjOracle.cpuGlobalScore(area_score.getText()));
 	}
 	
-	public void testStart3(){
-		assertEquals(State.START,currentState());
+	/*
+	 * Transition 8
+	 */
+	public void testTransition8(){
+		int player_score;
 		
-		do {
-			startButton.click();
-			assertEquals(State.PLAY,currentState());
-			assertFalse(area.getText().isEmpty());
-		
-			startButton.click();
-			assertEquals(State.PLAY,currentState());
-			assertFalse(area.getText().isEmpty());
-			if (!BlackJackOracleUtil.blackJack(area.getText()))
-				assertTrue(field.getText().isEmpty());
-		}
-		while (BlackJackOracleUtil.blackJack(area.getText()));
-	}
-	
-	public void testStart4(){
-		assertEquals(State.START,currentState());
-		
-		do {
-			startButton.click();
-			assertEquals(State.PLAY,currentState());
-			assertFalse(area.getText().isEmpty());
-		
-			startButton.click();
-			assertEquals(State.PLAY,currentState());
-			assertFalse(area.getText().isEmpty());
-			if (BlackJackOracleUtil.blackJack(area.getText()))
-				assertEquals("Blackjack",field.getText());
-		}
-		while (!BlackJackOracleUtil.blackJack(area.getText()));
-		
-	}
-	
-	public void testHit1(){
-		assertEquals(State.START,currentState());	
-		do{
-			startButton.click();
+		do {			
+			testTransition1();
 			assertEquals(State.PLAY,currentState());
 			
+			player_score=BjOracle.playerScore(area.getText());
 			hitButton.click();
-			if (!BlackJackOracleUtil.playerBusted(area.getText()))
-				assertEquals(State.PLAY,currentState());
 		}
-		while (BlackJackOracleUtil.playerBusted(area.getText()));
+		while (BjOracle.playerBusted(area.getText()) || BjOracle.blackJack(area.getText()));
+		
+		assertEquals(State.PLAY,currentState());
+		assertTrue(player_score<BjOracle.playerScore(area.getText()));
 	}
 	
-	public void testHit2(){
-		assertEquals(State.START,currentState());
-		do{
-			startButton.click();
+	/*
+	 * Transition 9
+	 */
+	public void testTransition9(){
+		int cpu_global_score;
+		do {
+			testTransition1();
 			assertEquals(State.PLAY,currentState());
 			
+			cpu_global_score=BjOracle.playerScore(area.getText());
 			hitButton.click();
-			if (BlackJackOracleUtil.playerBusted(area.getText()))
-				assertEquals(State.START,currentState());
 		}
-		while (!BlackJackOracleUtil.playerBusted(area.getText()));
-		assertEquals("Busted!",field.getText());
+		while (!BjOracle.playerBusted(area.getText()) || BjOracle.blackJack(area.getText()));
+		assertEquals(State.INIT,currentState());
+		assertEquals(BjOracle.bustedString,field.getText());
+		assertEquals(cpu_global_score+1,BjOracle.cpuGlobalScore(area_score.getText()));
 	}
 	
-	public void testStand1(){
-		assertEquals(State.START,currentState());
-		do{
-			startButton.click();
+	/*
+	 * Transition 10
+	 */
+	public void testTransition10(){
+		int player_score;
+		do {
+			testTransition1();
 			assertEquals(State.PLAY,currentState());
-
-			standButton.click();
-			assertEquals(State.START,currentState());
+			
+			player_score=BjOracle.playerScore(area.getText());
+			hitButton.click();
 		}
-		while (BlackJackOracleUtil.cpuWon(area.getText()));
-		assertEquals("Win!",field.getText());
+		while (BjOracle.playerBusted(area.getText()) || !BjOracle.blackJack(area.getText()));
+		assertEquals(State.PLAY,currentState());
+		assertEquals(BjOracle.bjString,field.getText());
+		assertTrue(player_score<BjOracle.playerScore(area.getText()));
 	}
 	
-	public void testStand2(){
-		assertEquals(State.START,currentState());
-		do{
-			startButton.click();
+	/*
+	 * Transition 11
+	 */
+	public void testTransition11(){
+		int player_global_score;
+		int cpu_global_score;
+		do {
+			testTransition1();
 			assertEquals(State.PLAY,currentState());
-
+			
+			player_global_score=BjOracle.playerGlobalScore(area_score.getText());
+			cpu_global_score=BjOracle.cpuGlobalScore(area_score.getText());
 			standButton.click();
-			assertEquals(State.START,currentState());
 		}
-		while (!BlackJackOracleUtil.cpuWon(area.getText()));
-		assertEquals("Lose!",field.getText());
+		while ( BjOracle.cpuWins(area.getText()) );
+		assertEquals(State.INIT,currentState());
+		assertEquals(BjOracle.winString,field.getText());
+		assertEquals(player_global_score+1, 
+				BjOracle.playerGlobalScore(area_score.getText()));
+		assertEquals(cpu_global_score,
+				BjOracle.cpuGlobalScore(area_score.getText()));
 	}
 	
+	/*
+	 * Transition 12
+	 */
+	public void testTransition12(){
+		int player_global_score;
+		int cpu_global_score;
+		do {
+			testTransition1();
+			assertEquals(State.PLAY,currentState());
+			
+			player_global_score=BjOracle.playerGlobalScore(area_score.getText());
+			cpu_global_score=BjOracle.cpuGlobalScore(area_score.getText());
+			standButton.click();
+		}
+		while ( !BjOracle.cpuWins(area.getText()) );
+		assertEquals(State.INIT,currentState());
+		assertEquals(BjOracle.loseString,field.getText());
+		assertEquals(player_global_score, 
+				BjOracle.playerGlobalScore(area_score.getText()));
+		assertEquals(cpu_global_score+1,
+				BjOracle.cpuGlobalScore(area_score.getText()));
+	}
 }
